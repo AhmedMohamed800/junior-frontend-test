5c0821b326363635e28caca9d1a683cfb5b9c1d7
Ahmed Mohamed <ahmedmoh0107@gmail.com>
2026-08-12 17:13:04 +0300

    feat(react-task-manager): create header and use the aside to create tasks

--- react-task-manager/src/App.tsx
+++ react-task-manager/src/App.tsx
@@ -1,10 +1,35 @@
+import CreateTask from "./components/CreateTask";
+import gsap from "gsap";
+import { useAside } from "./hooks/useAside";
+import Button from "./components/General/Button";
+import { useGSAP } from "@gsap/react";
+import { PlusIcon } from "./components/General/Icons";
+
+gsap.registerPlugin(useGSAP);
+
 function App() {
+  const { asideElement, toggleAside } = useAside();
+
   return (
     <>
-      <header className="flex justify-between">
-        <h1>Task Manager</h1>
-        <button>Create New Task</button>
+      <header className="container">
+        <div className="flex items-center justify-between py-4 border-b border-b-border  ">
+          <h1 className="text-2xl text-white font-semibold">Task Manager</h1>
+
+          <Button
+            text="Create New Task"
+            style="blue"
+            icon={<PlusIcon />}
+            onClick={() => {
+              toggleAside();
+            }}
+          />
+        </div>
       </header>
+
+      <CreateTask ref={asideElement} toggleAside={toggleAside} />
+
+      <main></main>
     </>
   );
 }
