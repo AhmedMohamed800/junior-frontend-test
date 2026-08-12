92ac642aa0da3998da30d5f70cb991739f3a870e
Ahmed Mohamed <ahmedmoh0107@gmail.com>
2026-08-12 16:15:22 +0300

    chore(react-task-manager): install and configure tailwindcss

--- react-task-manager/src/App.tsx
+++ react-task-manager/src/App.tsx
@@ -1,7 +1,10 @@
 function App() {
   return (
     <>
-      <p>Boilerplate</p>
+      <header className="flex justify-between">
+        <h1>Task Manager</h1>
+        <button>Create New Task</button>
+      </header>
     </>
   );
 }
