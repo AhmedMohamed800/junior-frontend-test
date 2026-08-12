16fa55293c14b3dbb0f70154de8eb765e3f80437
AhmedMohamed800 <ahmedmoh0107@gmail.com>
2026-08-13 00:58:21 +0300

    style(react-native-user-list): put styles in global file to access it everywhere

---
+++ react-native-user-list/src/styles/global.ts
@@ -0,0 +1,16 @@
+import { StyleSheet } from "react-native";
+
+export const colors = {
+  primary: "#0f62fe",
+  secondary: "#262626",
+  background: "#161616",
+  border: "#393939",
+};
+
+export const globalStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "flex-start",
+  },
+});
