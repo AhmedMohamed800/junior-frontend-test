59d654bd4c8fd9e171e94a7c2dfe79bf8b1ebca9
Ahmed Mohamed <ahmedmoh0107@gmail.com>
2026-08-13 13:39:46 +0300

    feat(react-native-user-list): create UserCard Component

--- react-native-user-list/src/components/UserCard.tsx
+++ react-native-user-list/src/components/UserCard.tsx
@@ -1,18 +1,25 @@
 import type { User } from "@/types/user";
+import { memo } from "react";
+
+import { globalStyles } from "@/styles/global";
 import { Text, View } from "react-native";
 
 interface UserCardProps {
   user: User;
 }
 
-export default function UserCard({ user }: UserCardProps) {
-  const address = `${user.address.street}, ${user.address.city}, ${user.address.zipcode}`;
-
+function UserCard({ user }: UserCardProps) {
   return (
-    <View>
-      <Text>{user.name}</Text>
-      <Text>{user.email}</Text>
-      <Text>{address}</Text>
+    <View style={globalStyles.userCard}>
+      <Text style={globalStyles.userCardBoldText}>{user.name}</Text>
+      <Text style={globalStyles.userCardNormalText}>{user.email}</Text>
+      <Text style={globalStyles.userCardNormalText}>
+        {user?.address
+          ? `${user.address.street}, ${user.address.city}, ${user.address.zipcode}`
+          : "No address"}
+      </Text>
     </View>
   );
 }
+
+export default memo(UserCard);
